b552dfe052760e7b35ecbef221f7e8051305f85b
phitranpt <phi.tran.pt@gmail.com>
2020-03-22 14:42:29 -0500

    added axios call with async await

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 
+        'Client-ID tFL2VYQdyRhVSqfRC1os0KvXJlPxvMH7JXKNigIEE48'
+      }
+    });
+    console.log(response.data.results);
   }
 
   render() {
